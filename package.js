e9562d0edd67d429f62ae76933270f80ee95debd
Abigail Watson <awatson1978@gmail.com>
2013-10-01 18:59:18 -0400

    added fonts directory to keep format the same as other font packages

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-    summary: "Meteorite package for adding handwritten script fonts to Meteor apps (not cursive fonts)."
+    summary: "Meteorite package for adding various calligraphy fonts to Meteor apps (including cursive fonts)."
 });
 
 Package.on_use(function (api) {
-    api.add_files('AlexBrush-Regular.ttf', "client");
+    api.add_files('fonts/AlexBrush-Regular.ttf', "client");
     api.add_files('fonts-calligraphy.css', "client");
 });
